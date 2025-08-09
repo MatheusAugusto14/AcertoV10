71c1bd110866d738a0be3369d621a9c870db6876
MatheusAugusto14 <matheusaugustodb@gmail.com>
2025-08-09 16:35:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,25 @@
-const CACHE_NAME = 'reckoning-v1';
-const FILES = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', evt=>{ evt.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))); self.skipWaiting(); });
-self.addEventListener('activate', evt=>{ evt.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE_NAME?caches.delete(k):null)))); self.clients.claim(); });
-self.addEventListener('fetch', evt=>{ evt.respondWith(caches.match(evt.request).then(r=> r || fetch(evt.request))); });
+const CACHE_NAME = 'sam-cache-v1';
+const FILES = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(caches.match(evt.request).then((res) => {
+    return res || fetch(evt.request);
+  }));
+});
